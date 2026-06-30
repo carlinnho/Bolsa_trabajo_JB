d46436590abb97d8fe6f82d67b86c339085d26dc
Carlinnho <carlospure2011@gmail.com>
2026-06-30 12:43:03 -0500

    header y cambios en app.jsx para páginas sin header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+
+function MainLayout() {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen bg-slate-50">
+        <Outlet />{" "}
+      </main>
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Esta será la página principal que hará tu compañero */}
-        <Route path="/" element={<div className="p-10 text-center">Página Principal</div>} />
-        
-        {/* Esta es tu página de Login */}
-        <Route path="/login" element={<Login />} />
+        <Route element={<MainLayout />}>
+          <Route
+            path="/"
+            element={<div className="p-10 text-center">Página Principal</div>}
+          />
+        </Route>
+
+        <Route
+          path="/login"
+          element={
+            <main className="min-h-screen bg-slate-50">
+              <Login />
+            </main>
+          }
+        />
       </Routes>
     </Router>
   );
